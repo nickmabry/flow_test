44b208c825c38f88d512c8d2f38ba16921d0a3c8
Nick Mabry <nicholasmabry@gmail.com>
2018-07-20 23:54:26 -0600

    Initial commit.

---
+++ lib/scanner.ex
@@ -0,0 +1,18 @@
+defmodule Scanner do
+  @moduledoc """
+  Documentation for Scanner.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scanner.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
